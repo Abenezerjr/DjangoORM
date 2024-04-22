5c626a9701f9300e5dd1aeb9ced470a73d879b82
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2024-04-22 23:58:42 +0300

    add model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,37 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Restaurant(models.Model):
+    class TypeChoices(models.TextChoices):
+        INDIAN='IN',"Indian"
+        CHINESE='Ch',"Chinese"
+        ITALIAN='IT',"Italian"
+        GREEK='GR',"Greek"
+        FASTFOOD='FF',"Fast Food"
+        OTHER='OT',"Other"
+    name=models.CharField(max_length=200)
+    website=models.URLField(default='')
+    date_opened=models.DateTimeField()
+    latitude=models.FloatField()
+    longitude=models.FloatField()
+    restauranet_type=models.CharField(max_length=2,choices=TypeChoices.choices)
+
+
+    def __str__(self):
+        return self.name
+
+class Rating(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    restaurant=models.ForeignKey(Restaurant,on_delete=models.CASCADE)
+    rating=models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.rating
+
+
+class Sale(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.SET_NULL,null=True)
+    income=models.DecimalField(max_digits=10,decimal_places=2)
+    datetime=models.DateTimeField(auto_now_add=True)
